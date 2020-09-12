679988ab2ed9261549fac297133d0349c34ccb2d
HARUKA ito <haruka_ito@keio.jp>
2020-09-13 03:45:03 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by 伊藤　陽香 on 2020/09/13.
+//  Copyright © 2020 伊藤　陽香. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
